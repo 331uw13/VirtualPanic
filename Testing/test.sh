db1bb4000d3878055fde7d6083cd508aaa492a2b
331uw13 <69628281+331uw13@users.noreply.github.com>
2021-01-30 19:10:00 +0000

    Delete test.sh

--- Testing/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if (cd ../ && exec ./build.sh debug install) ; then
-	if g++ main.cpp -Wl,-rpath=/usr/local/lib/ -lVirtualPanic -lSDL2 -lGL -lGLEW ; then
-		echo "Done!"
-		./a.out
-		#gdb -ex='set confirm on' -ex=run -ex=quit --args ./a.out		
-	fi
-fi
